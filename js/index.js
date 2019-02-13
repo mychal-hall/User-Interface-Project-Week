558a5558576b79917de572a4e0ffceead88314da
Mychal Hall <hallmychal@gmail.com>
2019-02-12 23:36:41 -0600

    Mobile Home page complete

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // JS goes here
 
 const menuButton = document.querySelector('.menuButton');
-menuButton.addEventListener('click', (event) => menuButton.classList.toggle("open"))
+const barOne = document.querySelector(".bar1");
+const barTwo = document.querySelector(".bar2");
+const barThree = document.querySelector(".bar3");
+menuButton.addEventListener('click', (event) => {menuButton.classList.toggle("open")})
