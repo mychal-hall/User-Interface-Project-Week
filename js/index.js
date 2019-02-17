73156327e191245f2d2cf03cff6a2f8315c951a0
Mychal Hall <hallmychal@gmail.com>
2019-02-17 00:59:30 -0500

    MVP Complete

--- js/index.js
+++ js/index.js
@@ -1,10 +1,49 @@
 // JS goes here
 
-const menuButton = document.querySelector('.menuButton');
-const navMenu = document.querySelector('.links');
-const barOne = document.querySelector(".bar1");
-const barTwo = document.querySelector(".bar2");
-const barThree = document.querySelector(".bar3");
-menuButton.addEventListener('click', (event) => {menuButton.classList.toggle("open")})
-menuButton.addEventListener('click', (event) => (navMenu.classList.toggle('hidden')))
+const menuButton = document.querySelector(".menuButton");
+const navMenu = document.querySelector(".links");
 
+menuButton.addEventListener("click", event => {
+  menuButton.classList.toggle("open");
+});
+menuButton.addEventListener("click", event =>
+  navMenu.classList.toggle("hidden")
+);
+
+class TabLink {
+  constructor(tablink) {
+    this.tablink = tablink;
+    this.data = this.tablink.dataset.tab;
+    this.itemElement = document.querySelector(
+      `.tabs-item[data-tab="${this.data}"]`
+    );
+    this.tabItem = new TabItem(this.itemElement);
+    this.tablink.addEventListener("click", () => {
+      this.select();
+    });
+  }
+
+  select() {
+    const links = document.querySelectorAll(".tabs-link");
+    Array.from(links).forEach(link => {
+      link.classList.remove("tabs-link-selected");
+    });
+    this.tablink.classList.add("tabs-link-selected");
+    this.tabItem.select();
+  }
+}
+class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    const items = document.querySelectorAll(".tabs-item");
+    items.forEach(item => item.classList.remove("tabs-item-selected"));
+    this.element.classList.add("tabs-item-selected");
+  }
+}
+
+links = document
+  .querySelectorAll(".tabs-link")
+  .forEach(tablink => new TabLink(tablink));
