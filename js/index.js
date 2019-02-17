88b17dd8b29e29d0c286c038c242969e7d3441c7
Mychal Hall <hallmychal@gmail.com>
2019-02-16 19:48:47 -0500

    Navigation Complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 // JS goes here
 
 const menuButton = document.querySelector('.menuButton');
+const navMenu = document.querySelector('.links');
 const barOne = document.querySelector(".bar1");
 const barTwo = document.querySelector(".bar2");
 const barThree = document.querySelector(".bar3");
 menuButton.addEventListener('click', (event) => {menuButton.classList.toggle("open")})
+menuButton.addEventListener('click', (event) => (navMenu.classList.toggle('hidden')))
+
